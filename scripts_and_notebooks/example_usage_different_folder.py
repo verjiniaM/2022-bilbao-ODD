66fd162929420c754f2be8b30ddf93abd9ffb29b
ASPP Student <student@localhost>
2022-09-07 11:21:28 +0200

    editable install of brewing package added

--- scripts_and_notebooks/example_usage_different_folder.py
+++ scripts_and_notebooks/example_usage_different_folder.py
@@ -1,1 +1,7 @@
 # Task: import make_example_potion from the module brew_potions.py
+
+from brewing.brew_potions import make_example_potion
+import brewing.brew_potions as br
+
+make_example_potion("Antonia")
+br.make_example_potion("Verji")
